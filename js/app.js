5e74aaa016c86d1c568f0a5c2ba55dbb60bd5128
manaressam9 <47770516+manaressam9@users.noreply.github.com>
2020-12-19 16:45:27 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,16 +2,14 @@
 let sections = document.querySelectorAll('section');
 let uOrderedList = document.getElementById('navbar__list');
 let frag = document.createDocumentFragment();
-let aLinks = document.querySelectorAll('a');
+
 
 //functions
 //remove 'active' class from all sections
 function removeActives(){
     sections.forEach((section) => {section.classList.remove('active');})
 }
-function removeActivesLinks(){
-    aLinks.forEach((a) => {a.classList.remove('activeLink');})
-}
+
 //set nav dynamically
 sections.forEach((section) => 
 {
@@ -25,6 +23,7 @@ sections.forEach((section) =>
     //connect the link and the section
    nListItem.addEventListener('click', function(){
        section.scrollIntoView({'behavior':'smooth'});
+       
       
    });
 
@@ -38,17 +37,18 @@ sections.forEach((section) => {
   let Top = rect.top;
   if( Top >=0 &&  Top <= 110 ){
     removeActives(); 
-   section.classList.add('active'); 
-   // connect the link in the navbar with the active section, and add class'activeLink' to the link<a></a>
-      //m4 radyaaaa t4t8l !!!
-   aLinks.forEach((a) => {
-    if(a.textContent == section.getAttribute('data-nav')){
-          removeActivesLink();
-          a.classList.add('activeLink');
-      };
-    
- });
-  };
+    section.classList.add('active');
+   // connect the link in the navbar with the active section, and add class'activeLink' to the link<a></a> !!WORKS!!
+   let Links = document.querySelectorAll('li');
+    Links.forEach((li) => {
+      let txt = li.textContent;
+      if (txt == section.getAttribute('data-nav')){
+        Links.forEach((li) => {li.classList.remove('activeLink');})
+        li.classList.add('activeLink');
+      }
+    })
+  
+  }
   
 })
 })
